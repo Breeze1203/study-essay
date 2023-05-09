cf8b2c89bb21d1d64755ce3d7166c6753610894b
pt <qq.com>
2023-05-09 22:04:15 +0800

    完成修改position的业务逻辑

--- vhr-server/src/main/java/org/javaboy/vhr/service/PositionService.java
+++ vhr-server/src/main/java/org/javaboy/vhr/service/PositionService.java
@@ -3,6 +3,7 @@ package org.javaboy.vhr.service;
 import jakarta.annotation.Resource;
 import org.javaboy.vhr.bean.Position;
 import org.javaboy.vhr.mapper.PositionMapper;
+import org.javaboy.vhr.utils.StatusUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,15 +19,23 @@ public class PositionService {
     @Resource(name = "PositionMapper")
     PositionMapper positionMapper;
 
+    //查找职位
     public List<Position> getAllPosition(){
         return positionMapper.getAllPosition();
     }
 
+    // 添加职位
     public int insertPos(Position position){
         return positionMapper.insertSelective(position);
     }
 
+    // 删除职位
     public int deleteByPrimaryKey(Integer id){
         return positionMapper.deleteByPrimaryKey(id);
+    }
+
+    // 修改职位
+    public int upDatePosition(Position position){
+       return positionMapper.updateByPrimaryKeySelective(position);
     }
 }
