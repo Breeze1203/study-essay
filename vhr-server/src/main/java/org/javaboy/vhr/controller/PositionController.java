7db059a14d468c58a083ab2b83b456aa6e742137
pt <qq.com>
2023-05-09 22:03:11 +0800

    添加修改职位的Controller

--- vhr-server/src/main/java/org/javaboy/vhr/controller/PositionController.java
+++ vhr-server/src/main/java/org/javaboy/vhr/controller/PositionController.java
@@ -1,6 +1,5 @@
 package org.javaboy.vhr.controller;
 
-import jakarta.annotation.Resource;
 import org.javaboy.vhr.bean.Position;
 import org.javaboy.vhr.service.PositionService;
 import org.javaboy.vhr.utils.StatusUtils;
@@ -43,16 +42,30 @@ public class PositionController {
     // 添加职位
     @PostMapping("/addPosition")
     public StatusUtils addPosition(@RequestParam("name") String name){
-        StatusUtils statusUtils=new StatusUtils();
         Position position = new Position();
         position.setName(name);
         position.setCreateDate(new Date());
+        StatusUtils statusUtils = new StatusUtils();
         int i = positionService.insertPos(position);
         if(i==1){
-            statusUtils.setStatus(200);
             statusUtils.setMessage("添加成功");
         }else {
-            System.out.println("添加失败");
+           statusUtils.setMessage("数据异常，请稍候再试");
+        }
+        return statusUtils;
+    }
+
+    @PostMapping("/updatePosition")
+    public StatusUtils addPosition(@RequestParam("name") String name,@RequestParam("id") Integer id){
+        Position position = new Position();
+        position.setName(name);
+        position.setId(id);
+        int i = positionService.upDatePosition(position);
+        StatusUtils statusUtils = new StatusUtils();
+        if(i==1){
+            statusUtils.setMessage("修改成功");
+        }else {
+            statusUtils.setMessage("数据异常，请稍后再试");
         }
         return statusUtils;
     }
