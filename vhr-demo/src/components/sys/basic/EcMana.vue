90c5025096e97b2bad13e4968ab5c394083a9ada
pt <qq.com>
2023-05-09 22:01:51 +0800

    完成前端修改职位的组件

--- vhr-demo/src/components/sys/basic/EcMana.vue
+++ vhr-demo/src/components/sys/basic/EcMana.vue
@@ -10,7 +10,7 @@
     <el-button type="primary" size="default" icon="Plus" @click="addPosition">添加</el-button>
   </div>
   <div style="margin-top: 15px">
-    <el-table :data="pos.tableData" border stripe style="width: 80%">
+    <el-table :data="pos.tableData" @selection-change="handleSelectionChange" border stripe style="width: 80%">
       <el-table-column type="selection" width="55"/>
       <el-table-column prop="id" label="编号" width="180"/>
       <el-table-column prop="name" label="职位" width="180"/>
@@ -32,6 +32,31 @@
       </el-table-column>
     </el-table>
   </div>
+  <div style="margin-top: 15px">
+    <el-button
+        @click="deleteIds"
+        size="small"
+        type="danger">批量删除
+    </el-button>
+  </div>
+  <div>
+    <el-dialog
+        v-model="pos.dialogShow"
+        title="修改职位"
+        width="40%">
+      <span>
+        <el-tag size="large">修改职位</el-tag>
+        <el-input size="default" style="width: 300px;margin-left: 15px" v-model="pos.tagPosition"></el-input>
+      </span>
+      <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="CloseDialog">取消</el-button>
+        <el-button type="primary" @click="updatePosition">确认</el-button>
+      </span>
+      </template>
+    </el-dialog>
+  </div>
+
 
 </template>
 
@@ -45,7 +70,12 @@ export default {
     return {
       pos: {
         name: "",
-        tableData: []
+        tableData: [],
+        dialogShow: false,
+        tagPosition: '',
+        id: '',
+        // 批量删除的ids
+        ids:[]
       }
     }
   },
@@ -59,10 +89,10 @@ export default {
   },
   methods: {
     // 添加职位
-    addPosition(){
-      if(this.pos.name){
-        request.addPosition(this.pos.name).then(res=>{
-          if(res){
+    addPosition() {
+      if (this.pos.name) {
+        request.addPosition(this.pos.name).then(res => {
+          if (res) {
             request.positionInit().then(res => {
               if (res) {
                 this.pos.tableData = res.data;
@@ -70,8 +100,8 @@ export default {
             })
           }
         })
-        this.pos.name='';
-      }else {
+        this.pos.name = '';
+      } else {
         ElMessage({
           showClose: true,
           message: '请输入要添加的职位',
@@ -80,9 +110,10 @@ export default {
       }
     }
     ,
-    handleDelete(index,data){
+    // 删除职位
+    handleDelete(index, data) {
       ElMessageBox.confirm(
-          '此操作将删除职位'+data.name+',是否继续?',
+          '此操作将删除职位' + data.name + ',是否继续?',
           {
             distinguishCancelAndClose: true,
             confirmButtonText: '确定',
@@ -90,8 +121,8 @@ export default {
           }
       )
           .then(() => {
-            request.deletePositionById(data.id).then(res=>{
-              if(res){
+            request.deletePositionById(data.id).then(res => {
+              if (res) {
                 request.positionInit().then(res => {
                   if (res) {
                     this.pos.tableData = res.data;
@@ -103,11 +134,46 @@ export default {
           .catch(() => {
             ElMessage({
               type: 'info',
-              message:'已取消操作'
+              message: '已取消操作'
             })
           })
+    },
+    // 修改职位
+    handleEdit(index, data) {
+      this.pos.tagPosition = data.name;
+      this.pos.id = data.id;
+      this.pos.dialogShow = true;
+    },
+    CloseDialog() {
+      this.pos.dialogShow = false;
+    },
+    updatePosition() {
+      request.update(this.pos.id, this.pos.tagPosition).then(res => {
+        if (res) {
+          if (res) {
+            request.positionInit().then(res => {
+              if (res) {
+                this.pos.tableData = res.data;
+                this.pos.dialogShow = false;
+              }
+            })
+          }
+        }
+      })
+    },
+    // 批量删除 有个回调函数参数值是选中那行表格数据
+    handleSelectionChange(val) {
+      this.pos.ids = [];
+      for (let i = 0; i < val.length; i++) {
+        this.pos.ids.push(val[i].id);
+      }
+      console.log(this.pos.ids);
     }
-}
+    ,
+    deleteIds(){
+      console.log(this.pos.ids);
+  }
+  }
 }
 </script>
 
