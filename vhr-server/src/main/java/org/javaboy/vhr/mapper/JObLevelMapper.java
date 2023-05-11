671b7e7d3a6cc92d7bb1eb4a22b9d21a38ad674c
pt <qq.com>
2023-05-11 20:28:32 +0800

    完成职称管理mapper文件

--- vhr-server/src/main/java/org/javaboy/vhr/mapper/JObLevelMapper.java
+++ vhr-server/src/main/java/org/javaboy/vhr/mapper/JObLevelMapper.java
@@ -1,17 +1,27 @@
 package org.javaboy.vhr.mapper;
 
 import org.javaboy.vhr.bean.JObLevel;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository("JobLevelMapper")
 public interface JObLevelMapper {
+    // 删除职位
     int deleteByPrimaryKey(Integer id);
 
-    int insert(JObLevel record);
 
+    // 添加职位
     int insertSelective(JObLevel record);
 
-    JObLevel selectByPrimaryKey(Integer id);
 
+    // 更新职称
     int updateByPrimaryKeySelective(JObLevel record);
+    
 
-    int updateByPrimaryKey(JObLevel record);
+    // 查询所有职称
+    List<JObLevel> getAllJobLevel();
+
+    //批量删除
+    int deleteJobLevelByIds(Integer[] ids);
 }
