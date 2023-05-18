c9593c62d11864149a344125640f1d5bc4ab1361
pt <qq.com>
2023-05-18 20:57:41 +0800

    角色可访问资源的业务层

---
+++ vhr-server/src/main/java/org/javaboy/vhr/service/RoleService.java
@@ -0,0 +1,23 @@
+package org.javaboy.vhr.service;
+
+import jakarta.annotation.Resource;
+import org.javaboy.vhr.bean.Role;
+import org.javaboy.vhr.mapper.RoleMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author Breeze
+ * @address Shenzhen China
+ * @email 3548297839@qq.com
+ */
+@Service(value = "RoleService")
+public class RoleService {
+    @Resource(name = "RoleMapper")
+    RoleMapper roleMapper;
+
+    public List<Role> getAllRoles(){
+        return roleMapper.getAllRoles();
+    }
+}
