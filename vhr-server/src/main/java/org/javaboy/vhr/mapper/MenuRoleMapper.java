b3c46b91f4262bba551afcb8055a1a834fd7b428
pt <qq.com>
2023-05-29 23:26:22 +0800

    添加更改role所能访问路由的sql映射接口

--- vhr-server/src/main/java/org/javaboy/vhr/mapper/MenuRoleMapper.java
+++ vhr-server/src/main/java/org/javaboy/vhr/mapper/MenuRoleMapper.java
@@ -1,7 +1,9 @@
 package org.javaboy.vhr.mapper;
 
 import org.javaboy.vhr.bean.MenuRole;
+import org.springframework.stereotype.Repository;
 
+@Repository("MenuRoleMapper")
 public interface MenuRoleMapper {
     int deleteByPrimaryKey(Integer id);
 
@@ -14,4 +16,10 @@ public interface MenuRoleMapper {
     int updateByPrimaryKeySelective(MenuRole record);
 
     int updateByPrimaryKey(MenuRole record);
+
+    // 角色关联表插入数据
+    int insertRoleMenu(Integer[] mids,int rid);
+
+    // 删除角色所拥有的菜单
+    int deleteMenuByRole(int rid);
 }
