54301f11aa6917cc41a692abc0ad7d63c16610e8
pt <qq.com>
2023-05-29 23:28:28 +0800

    更改成员变量的命名

--- vhr-server/src/main/java/org/javaboy/vhr/bean/Department.java
+++ vhr-server/src/main/java/org/javaboy/vhr/bean/Department.java
@@ -1,17 +1,29 @@
 package org.javaboy.vhr.bean;
 
+import java.util.List;
+
 public class Department {
     private Integer id;
 
     private String name;
 
-    private Integer parentid;
+    private Integer parentId;
 
-    private String deppath;
+    private String depPath;
 
     private Boolean enabled;
 
-    private Boolean isparent;
+    private Boolean isParent;
+
+    private List<Department> children;
+
+    public List<Department> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Department> children) {
+        this.children = children;
+    }
 
     public Integer getId() {
         return id;
@@ -29,20 +41,20 @@ public class Department {
         this.name = name == null ? null : name.trim();
     }
 
-    public Integer getParentid() {
-        return parentid;
+    public Integer getParentId() {
+        return parentId;
     }
 
-    public void setParentid(Integer parentid) {
-        this.parentid = parentid;
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
     }
 
-    public String getDeppath() {
-        return deppath;
+    public String getDepPath() {
+        return depPath;
     }
 
-    public void setDeppath(String deppath) {
-        this.deppath = deppath == null ? null : deppath.trim();
+    public void setDepPath(String depPath) {
+        this.depPath = depPath;
     }
 
     public Boolean getEnabled() {
@@ -53,11 +65,24 @@ public class Department {
         this.enabled = enabled;
     }
 
-    public Boolean getIsparent() {
-        return isparent;
+    public Boolean getParent() {
+        return isParent;
     }
 
-    public void setIsparent(Boolean isparent) {
-        this.isparent = isparent;
+    public void setParent(Boolean parent) {
+        isParent = parent;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", parentId=" + parentId +
+                ", depPath='" + depPath + '\'' +
+                ", enabled=" + enabled +
+                ", isParent=" + isParent +
+                ", children=" + children +
+                '}';
     }
 }
