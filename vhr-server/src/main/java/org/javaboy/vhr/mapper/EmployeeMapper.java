2a994831d379a5b71c25922ad670dd66dccc93a0
pt <qq.com>
2023-06-02 20:35:36 +0800

    添加根据部门id查找员工的映射接口

--- vhr-server/src/main/java/org/javaboy/vhr/mapper/EmployeeMapper.java
+++ vhr-server/src/main/java/org/javaboy/vhr/mapper/EmployeeMapper.java
@@ -1,7 +1,11 @@
 package org.javaboy.vhr.mapper;
 
 import org.javaboy.vhr.bean.Employee;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository("EmployeeMapper")
 public interface EmployeeMapper {
     int deleteByPrimaryKey(Integer id);
 
@@ -14,4 +18,7 @@ public interface EmployeeMapper {
     int updateByPrimaryKeySelective(Employee record);
 
     int updateByPrimaryKey(Employee record);
+
+    // 根据部门的parentId查找员工
+    List<Employee> findWorkerByDepartmentId(Integer departmentId);
 }
