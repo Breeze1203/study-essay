162cc0396fd4e2116afabb43e1adb7c4b02a5b00
pt <qq.com>
2023-06-13 17:21:36 +0800

    操作员管理模块的操作员所拥有角色的crud

--- vhr-server/src/main/java/org/javaboy/vhr/mapper/HrRoleMapper.java
+++ vhr-server/src/main/java/org/javaboy/vhr/mapper/HrRoleMapper.java
@@ -1,7 +1,9 @@
 package org.javaboy.vhr.mapper;
 
 import org.javaboy.vhr.bean.HrRole;
+import org.springframework.stereotype.Repository;
 
+@Repository("HrRoleMapper")
 public interface HrRoleMapper {
     int deleteByPrimaryKey(Integer id);
 
@@ -14,4 +16,10 @@ public interface HrRoleMapper {
     int updateByPrimaryKeySelective(HrRole record);
 
     int updateByPrimaryKey(HrRole record);
+
+    // 给用户绑定所选择的角色 先将之前的角色删除掉
+    int insertRoleByHr(Integer hrid,Integer[] rids);
+
+    // 重新绑定角色前，删除掉之前所具有的角色
+    int deleteHrRoleByHrid(Integer hrid);
 }
