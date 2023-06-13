1983551189b3b6632db9d82b7f75e83119000632
pt <qq.com>
2023-06-13 17:27:43 +0800

    处理登录及页面刷新问题

--- vhr-demo/src/main.js
+++ vhr-demo/src/main.js
@@ -1,7 +1,7 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 import store from './store'
-import router from "@/router";
+import router from "./router";
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css';
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
@@ -9,27 +9,23 @@ import {formatRoutes} from './utils/menus'
 // 导入font-awesome图标
 import 'font-awesome/css/font-awesome.min.css'
 import request from "@/api/request";
-import {ElMessage} from "element-plus";
 
 
 const app = createApp(App);
 router.beforeEach((to, from, next) => {
-    if (to.path ==='/') {
+    if (to.path === '/') {
         next()
     } else {
-        if(window.sessionStorage.getItem("user")===null){
-            ElMessage.error("尚未登录，请登录");
-            router.replace("/");
-        }
-        if (store.state.routes.length=== 0) {
+        if (store.state.routes.length === 0) {
             request.menuinit().then(resp => {
-                let b = formatRoutes(resp.data);
+                var b = formatRoutes(resp.data);
                 // 将查询到的菜单数据存入store中
                 store.commit('menu', b);
                 for (var i = 0; i < b.length; i++) {
-                    router.addRoute(b[i]);
+                    console.log(b[i].children);
+                    router.addRoute(b[i])
                 }
-                next({...to, replace: true})
+                next({...to, replace: true});
             })
         } else {
             next();
