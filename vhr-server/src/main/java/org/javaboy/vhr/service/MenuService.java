1ec238b60b9617431ea59e272ce91c5314cc2680
pt <qq.com>
2023-06-17 23:15:16 +0800

    菜单展示，去掉一级菜单

--- vhr-server/src/main/java/org/javaboy/vhr/service/MenuService.java
+++ vhr-server/src/main/java/org/javaboy/vhr/service/MenuService.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
