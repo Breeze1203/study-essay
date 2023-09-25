5229f1e977f26fb46313a786d574868dd40b1e35
pt <qq.com>
2023-09-25 20:01:09 +0800

    modifed

--- vhr/vhr-server/src/test/java/org/javaboy/vhr/VhrServerApplicationText.java
+++ vhr/vhr-server/src/test/java/org/javaboy/vhr/VhrServerApplicationText.java
@@ -1,10 +1,14 @@
 package org.javaboy.vhr;
 
 
+import org.javaboy.vhr.bean.Salary;
+import org.javaboy.vhr.mapper.SalaryMapper;
 import org.javaboy.vhr.rocketmq.ProducerMailService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 
 /**
@@ -13,10 +17,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class VhrServerApplicationText{
     @Autowired
+    SalaryMapper salaryMapper;
+
+    @Autowired
     ProducerMailService producerMailService;
 
     @Test
     void contextLoads() {
-//        producerMailService.send();
+        List<Salary> salaries = salaryMapper.selectAll();
+        for (Salary s:salaries) {
+            System.out.println(s.toString());
+        }
     }
 }
