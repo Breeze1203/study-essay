2ff19a70d808f70291a89b389cf2f63663ff0074
pt <qq.com>
2023-09-25 20:00:31 +0800

    属性日期格式的全局处理

--- vhr/vhr-server/src/main/java/org/javaboy/vhr/webconfig/DateConfig.java
+++ vhr/vhr-server/src/main/java/org/javaboy/vhr/webconfig/DateConfig.java
@@ -1,12 +1,30 @@
 package org.javaboy.vhr.webconfig;
-    /**
- * @projectName:    vhr 
- * @package:        org.javaboy.vhr.webconfig
- * @className:      Dateconfig
- * @author:     pt3548297839
- * @description:  TODO  
- * @date:    2023/9/25 19:39
- * @version:    1.0
- */ 
-public class Dateconfig {
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * @projectName: vhr
+ * @package: org.javaboy.vhr.webconfig
+ * @className: DateConfig
+ * @author: pt3548297839
+ * @description: TODO
+ * @date: 2023/9/25 19:39
+ * @version: 1.0
+ */
+@Configuration
+public class DateConfig {
+    @Bean
+    ObjectMapper objectMapper(){
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));
+        mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd"));
+        return mapper;
+    }
 }
